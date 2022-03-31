7f219051ba99387f69616ea88bbf60f198e4a76a
karaszewski21 <pkaraszewski@agora.pl>
2022-03-31 19:28:30 +0200

    first commit

---
+++ pages/contact/index.tsx
@@ -0,0 +1,28 @@
+import type { GetServerSideProps,  InferGetStaticPropsType,  NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+interface User {
+  name: string;
+  age: number;
+}
+
+interface Props {
+  users: User[];
+}
+
+export const Contact: NextPage<Props> = ({users}: Props) => {
+
+  const data = users.map((element,index) => (<p key={index}> {element.name}</p>))
+  return (<div>{data}</div>)
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+ 
+   const res = await fetch(`https://susana-api.herokuapp.com/`)
+   const users: User[]  = await res.json()
+   return { props: { users } }
+}
+
+
+export default Contact
---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import type { GetServerSideProps,  InferGetStaticPropsType,  NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+interface User {
+  name: string;
+  age: number;
+}
+
+interface Props {
+  users: User[];
+}
+
+export const Home: NextPage<Props> = ({users}: Props) => {
+
+  const data = users.map((element,index) => (<p key={index}> {element.name}</p>))
+  return (
+    <> 
+     <Link href="/contact">
+          <a  href="/contact">Contact</a>
+        </Link>
+    <div>{data}</div></>
+ )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+ 
+   const res = await fetch(`https://susana-api.herokuapp.com/`)
+   const users: User[]  = await res.json()
+   return { props: { users } }
+}
+
+
+export default Home
